6e0dece9b381be2d88396a31d045c7b321d66f50
Liz P <parisielizabeth@gmail.com>
2020-01-11 04:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,11 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    if params[:user][:password] != params[:user][:password_confirmation]
-      render :new
-    else @user.save
+    @user = User.create(user_params)
+    if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
     end
   end
 
